30626e7063f920ed8eaf2a52f5ebb3ba629e8db2
unknown <srivardhansamgoju@gmail.com>
2026-06-12 15:23:02 +0530

    Telemetry generation is done

--- configs/db_config.py
+++ configs/db_config.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 
 DATABASE_URL = (
-    "postgresql://admin:admin123@localhost:5432/cloudpulse_db"
+    "postgresql://admin:admin123@127.0.0.1:5433/cloudpulse_db"
 )
 
 engine = create_engine(
---
+++ ml/data_ingestion/postgres_ingestion.py
@@ -0,0 +1,71 @@
+import pandas as pd
+from sqlalchemy import text
+from tqdm import tqdm
+
+from configs.db_config import engine
+
+
+CSV_PATH = "data/raw/telemetry_data.csv"
+
+
+def load_csv():
+
+    print("Loading telemetry dataset...")
+
+    df = pd.read_csv(
+        CSV_PATH,
+        parse_dates=["timestamp"]
+    )
+
+    print(f"Dataset Loaded: {df.shape}")
+
+    return df
+
+
+def truncate_existing_data():
+
+    print("Clearing old telemetry data...")
+
+    with engine.connect() as conn:
+        conn.execute(
+            text(
+                "TRUNCATE TABLE telemetry_metrics RESTART IDENTITY;"
+            )
+        )
+        conn.commit()
+
+    print("Old data removed.")
+
+
+def ingest_to_postgres(df):
+
+    batch_size = 10000
+
+    print("\nStarting ingestion...")
+
+    for i in tqdm(
+        range(0, len(df), batch_size)
+    ):
+
+        batch = df.iloc[i:i + batch_size]
+
+        batch.to_sql(
+            "telemetry_metrics",
+            engine,
+            if_exists="append",
+            index=False,
+            method="multi"
+        )
+
+    print("\nIngestion completed successfully!")
+
+
+if __name__ == "__main__":
+
+    df = load_csv()
+
+    truncate_existing_data()
+
+    ingest_to_postgres(df)
+
+    print("\nCloudPulse telemetry loaded into PostgreSQL!")
